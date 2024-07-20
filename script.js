a7469c60d44bb8f814cc62088e9cd5b26159f371
alexandrafs5 <alexandra051009@gmail.com>
2024-07-20 13:16:08 -0700

    Add the clear button and the change size slider

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-const GRIDSIDE = 600;
-let side = 16;
+const GRIDSIZE = 600;
+const DEFAULT_SIDE = 16;
+
+let currentSide = DEFAULT_SIDE;
+
 let mouseDown = false;
 
+const sizeSlider = document.querySelector(".sizeSlider");
 const grid = document.querySelector(".grid");
+const clear = document.querySelector(".clear");
 
-for (let i = 0; i < (side * side); i++) {
-    const gridElement = document.createElement("div");
-    grid.appendChild(gridElement);
-
-    gridElement.classList.add("gridElement");
-    gridElement.style.width = `${(GRIDSIDE / side) - 2}px`;
-    gridElement.style.height = `${(GRIDSIDE / side) - 2}px`;
-
-    gridElement.addEventListener("mousedown",function() {
-        mouseDown = true;
-    });
-    gridElement.addEventListener("mouseover", addColor);  
-    gridElement.addEventListener("mouseup",function() {
-        mouseDown = false;
-    });
-    gridElement.addEventListener("click", addColor);  
+function createGrid(side) {
+    for (let i = 0; i < (side * side); i++) {
+        const gridElement = document.createElement("div");
+        grid.appendChild(gridElement);
+    
+        gridElement.classList.add("gridElement");
+        gridElement.style.width = `${(GRIDSIZE / side) - 2}px`;
+        gridElement.style.height = `${(GRIDSIZE / side) - 2}px`;
+    
+        gridElement.addEventListener("mousedown",function() {
+            mouseDown = true;
+        });
+        gridElement.addEventListener("mouseover", addColor);  
+        gridElement.addEventListener("mouseup",function() {
+            mouseDown = false;
+        });
+        gridElement.addEventListener("click", addColor);  
+    }
 }
+
+clear.addEventListener("click",function() {
+    reloadGrid();
+});
+
+sizeSlider.addEventListener("change", function() {
+    changeSize();
+})
 
 function addColor(e) {
     if(e.type === "mouseover" && mouseDown == true || e.type === "click") {
@@ -29,3 +44,15 @@ function addColor(e) {
         return;
     }
 }
+
+function reloadGrid() {
+    grid.innerHTML = null;
+    createGrid(currentSide);
+}
+
+function changeSize() {
+    currentSide = sizeSlider.value;
+    reloadGrid();
+}
+
+createGrid(DEFAULT_SIDE);
